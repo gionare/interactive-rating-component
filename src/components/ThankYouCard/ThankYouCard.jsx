30f4bfe799ab250e28965877e5430e2b8ab91290
Giorgi Nareklishvili <38813634+gionare@users.noreply.github.com>
2024-02-20 23:51:40 -0500

    add Thx image

--- src/components/ThankYouCard/ThankYouCard.jsx
+++ src/components/ThankYouCard/ThankYouCard.jsx
@@ -1,10 +1,11 @@
+import Thx from "../../assets/illustration-thank-you.svg";
 import React from "react";
 import "./ThankYouCard.css";
 
 export default function ThankYouCard(props) {
   return (
     <>
-      <img src="./public/illustration-thank-you.svg" alt="thank you" />
+      <img src={Thx} alt="thank you" />
       <p className="selected-score">You selected {props.rate} out of 5</p>
       <h2>Thank you!</h2>
       <p>
